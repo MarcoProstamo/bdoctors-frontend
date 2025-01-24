2a87a2187a5d99980e4cf8e92f653fb04ce869e9
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-24 16:03:50 +0100

    add fetch

--- src/assets/pages/AddDoctorPage.jsx
+++ src/assets/pages/AddDoctorPage.jsx
@@ -1,9 +1,36 @@
 import FormDoctor from "../components/FormDoctor";
+import { useEffect, useState } from "react";
 
 export default function AddDoctorPage() {
   const handleDoctorRegistration = (data) => {
     data.cellphone_number = "+39" + data.cellphone_number;
     console.log("Nuovo dottore registrato:", data);
+
+    const newDoctorData = {
+      name: data.name,
+      surname: data.surname,
+      email: data.email,
+      cellphone_number: data.cellphone_number,
+      address: data.address,
+      medical_specialization: data.medical_specialization,
+    };
+
+    fetch("http://localhost:3000/doctors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDoctorData),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Errore nella fetch`);
+        }
+        return res.json();
+      })
+      .catch((error) => {
+        console.error("Si è verificato un errore durante la fetch:", error);
+      });
   };
 
   return (

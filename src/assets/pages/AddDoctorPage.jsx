ecded06fb82bb059ebc5624d024e2f16ee700977
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-29 10:56:34 +0100

    Fix → Card Alignment in Add Up

--- src/assets/pages/AddDoctorPage.jsx
+++ src/assets/pages/AddDoctorPage.jsx
@@ -46,43 +46,43 @@ export default function AddDoctorPage() {
         <h4 className="text-center my-5">
           Registrati gratuitamente e raggiungi nuovi pazienti:
         </h4>
-        <div className="text-center ">
-          <div className="row row-cols-3 p-2 ">
-            <div className="col border p-4 ">
-              <p>
-                <i className="fa-solid fa-globe fa-2x"></i>
-              </p>
-              <p className="fw-bolder">Aumenta la tua visibilità online</p>
-              <p className="fw-light">
-                Fatti trovare facilmente dai pazienti nella tua area grazie a un
-                profilo professionale completo e ottimizzato. Connettiti con chi
-                ha bisogno delle tue competenze, ovunque tu sia.
-              </p>
-            </div>
-            <div className="col border p-4 ">
-              <p>
-                <i className="fa-solid fa-calendar-check fa-2x"></i>
-              </p>
-              <p className="fw-bolder">Agenda sempre piena e organizzata</p>
-              <p className="fw-light">
-                Ricevi nuove richieste di appuntamento direttamente online e
-                gestisci la tua agenda con facilità, risparmiando tempo e
-                aumentando la tua efficienza.
-              </p>
-            </div>
-            <div className="col border p-4 ">
-              <p>
-                <i className=" fa-solid fa-star fa-2x"></i>
-              </p>
-              <p className="fw-bolder">Costruisci fiducia e credibilità</p>
-              <p className="fw-light">
-                Mostra le tue qualifiche, recensioni dei pazienti e i tuoi
-                servizi per consolidare la tua reputazione e attrarre più
-                pazienti in cerca di esperti affidabili.
-              </p>
-            </div>
+
+        <div className="row text-center gap-3 justify-content-center">
+          <div className="col-lg-3 col-sm-12 border p-4 bg-white shadow">
+            <p>
+              <i className="fa-solid fa-globe fa-2x"></i>
+            </p>
+            <p className="fw-bolder">Aumenta la tua visibilità online</p>
+            <p className="fw-light">
+              Fatti trovare facilmente dai pazienti nella tua area grazie a un
+              profilo professionale completo e ottimizzato. Connettiti con chi
+              ha bisogno delle tue competenze, ovunque tu sia.
+            </p>
+          </div>
+          <div className="col-lg-3 col-sm-12 border p-4 bg-white shadow">
+            <p>
+              <i className="fa-solid fa-calendar-check fa-2x"></i>
+            </p>
+            <p className="fw-bolder">Agenda sempre piena e organizzata</p>
+            <p className="fw-light">
+              Ricevi nuove richieste di appuntamento direttamente online e
+              gestisci la tua agenda con facilità, risparmiando tempo e
+              aumentando la tua efficienza.
+            </p>
+          </div>
+          <div className="col-lg-3 col-sm-12 border p-4 bg-white shadow">
+            <p>
+              <i className=" fa-solid fa-star fa-2x"></i>
+            </p>
+            <p className="fw-bolder">Costruisci fiducia e credibilità</p>
+            <p className="fw-light">
+              Mostra le tue qualifiche, recensioni dei pazienti e i tuoi servizi
+              per consolidare la tua reputazione e attrarre più pazienti in
+              cerca di esperti affidabili.
+            </p>
           </div>
         </div>
+
         <h4 className="text-center my-5">Compila il form qui sotto:</h4>
         <section className=" mt-5 p-5 border shadow bg-white">
           <div>
